9817e400ee1ca6fbc85c86af27e00c212e393ad4
Xfurry <radu_cantor@yahoo.com>
2011-05-04 17:05:25 +0300

    Remove unused code

--- PAGES/DataMining.aspx.cs
+++ PAGES/DataMining.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.Clustering;
 using System.Data;
 
 namespace WebApplication_OLAP.classes
